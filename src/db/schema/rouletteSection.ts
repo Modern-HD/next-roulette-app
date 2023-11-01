1158894a3c112a26511bcab24ea54db5be8de5e6
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-01 22:02:24 +0900

    인덱스 추가

--- src/db/schema/rouletteSection.ts
+++ src/db/schema/rouletteSection.ts
@@ -1,17 +1,25 @@
-import { integer, pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { index, integer, pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
 import rouletteSet from './rouletteSet';
 
-const rouletteSection = pgTable('roulette_section', {
-    idx: serial('idx').primaryKey().notNull(),
-    roulette_set_idx: integer('roulette_set_idx')
-        .notNull()
-        .references(() => rouletteSet.idx)
-        .notNull(),
-    content: varchar('content').notNull(),
-    weight: integer('weight').default(10000).notNull(),
-    location: integer('location').notNull(),
-    created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
-    updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
-});
+const rouletteSection = pgTable(
+    'roulette_section',
+    {
+        idx: serial('idx').primaryKey().notNull(),
+        roulette_set_idx: integer('roulette_set_idx')
+            .notNull()
+            .references(() => rouletteSet.idx)
+            .notNull(),
+        content: varchar('content').notNull(),
+        weight: integer('weight').default(10000).notNull(),
+        location: integer('location').notNull(),
+        created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+        updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
+    },
+    table => ({
+        roulette_set_idx_created_at_key: index('roulette_set_idx_created_at_key')
+            .on(table.roulette_set_idx, table.created_at)
+            .concurrently(),
+    }),
+);
 
 export default rouletteSection;
--- src/db/schema/rouletteSet.ts
+++ src/db/schema/rouletteSet.ts
@@ -1,21 +1,28 @@
-import { boolean, integer, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { boolean, index, integer, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 import user from './user';
 import category from './category';
 
-const rouletteSet = pgTable('roulette_set', {
-    idx: serial('idx').primaryKey().notNull(),
-    title: varchar('title').notNull(),
-    description: text('description').notNull(),
-    user_idx: integer('user_idx')
-        .notNull()
-        .references(() => user.idx),
-    play_count: integer('play_count').notNull(),
-    category_idx: integer('category_idx')
-        .notNull()
-        .references(() => category.idx),
-    public: boolean('public').default(false).notNull(),
-    created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
-    updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
-});
+const rouletteSet = pgTable(
+    'roulette_set',
+    {
+        idx: serial('idx').primaryKey().notNull(),
+        title: varchar('title').notNull(),
+        description: text('description').notNull(),
+        user_idx: integer('user_idx')
+            .notNull()
+            .references(() => user.idx),
+        play_count: integer('play_count').notNull(),
+        category_idx: integer('category_idx')
+            .notNull()
+            .references(() => category.idx),
+        public: boolean('public').default(false).notNull(),
+        created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+        updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
+    },
+    table => ({
+        title_key: index('title_key').on(table.title),
+        user_idx_key: index('user_idx_key').on(table.user_idx),
+    }),
+);
 
 export default rouletteSet;
